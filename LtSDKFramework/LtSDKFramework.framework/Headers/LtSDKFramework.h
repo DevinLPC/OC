fe9f98b26da2cac7f1f301d9bcd912210ec98705
lipengchao <lipengchao@lantuxinxi.com>
2019-05-10 15:12:02 +0800

    0.0.1

---
+++ LtSDKFramework/LtSDKFramework.framework/Headers/LtSDKFramework.h
@@ -0,0 +1,20 @@
+//
+//  LtSDKFramework.h
+//  LtSDKFramework
+//
+//  Created by 李鹏超 on 2019/5/10.
+//  Copyright © 2019 李鹏超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "LtTool.h"
+
+//! Project version number for LtSDKFramework.
+FOUNDATION_EXPORT double LtSDKFrameworkVersionNumber;
+
+//! Project version string for LtSDKFramework.
+FOUNDATION_EXPORT const unsigned char LtSDKFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LtSDKFramework/PublicHeader.h>
+
+
