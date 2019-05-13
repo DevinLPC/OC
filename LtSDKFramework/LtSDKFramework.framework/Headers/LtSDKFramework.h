a2e5623e86a529bac57bd33f633cd830ce9dc4ce
lipengchao <lipengchao@lantuxinxi.com>
2019-05-13 10:34:46 +0800

    0.0.2

--- LtSDKFramework/LtSDKFramework.framework/Headers/LtSDKFramework.h
+++ LtSDKFramework/LtSDKFramework.framework/Headers/LtSDKFramework.h
@@ -2,12 +2,11 @@
 //  LtSDKFramework.h
 //  LtSDKFramework
 //
-//  Created by 李鹏超 on 2019/5/10.
-//  Copyright © 2019 李鹏超. All rights reserved.
+//  Created by Lz on 2019/5/10.
+//  Copyright © 2019 Lz. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
-#import "LtTool.h"
 
 //! Project version number for LtSDKFramework.
 FOUNDATION_EXPORT double LtSDKFrameworkVersionNumber;
